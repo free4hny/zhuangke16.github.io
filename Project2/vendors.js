a4477342a6408b69518e7dec59f75553a3ccfebc
Ke Zhuang <zhuangke16@gmail.com>
2017-10-31 23:10:09 -0400

    111
    111

---
+++ Project2/vendors.js
@@ -0,0 +1,8 @@
+require("classnames");
+require("react");
+require("react-dom");
+require("immutable");
+require("prop-types");
+require("react-immutable-render-mixin");
+require("d3");
+require("d3-selection-multi");
